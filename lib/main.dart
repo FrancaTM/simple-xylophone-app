da5655ecd1f95abe710781d0e29dec7f33ff2af4
Túlio <francatm@outlook.com>
2019-05-22 11:26:11 -0300

    Add capability for playing sounds

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 //Project based on - Xylophone - A Simple Musical Instrument App [https://www.udemy.com/flutter-bootcamp-with-dart/learn/lecture/14483184#questions/7129332]
 
 void main() {
@@ -6,12 +7,21 @@ void main() {
 }
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: FlatButton(
+              onPressed: () {
+                player.play('note1.wav');
+              },
+              child: Text('play'),
+            ),
+          ),
         ),
       ),
     );
